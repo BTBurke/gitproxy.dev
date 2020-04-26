bfdfa3df9b9852ff817b9c713c59980599bceaa4
Bryan Burke <bryan.t.burke@gmail.com>
2020-04-25 23:27:45 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Source Sans Pro', 'monospace'],
+      body: ['Source Sans Pro', 'monospace'],
+    },
+    extend: {
+      colors: {
+        crtgreen: '#00FF33',
+        black: '#111',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
